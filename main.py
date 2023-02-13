f87a7668a5516a08fa6368025661851a1b48a73c
Hafiz Maaz Anis <89415688+Maaz48@users.noreply.github.com>
2023-02-14 01:58:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 from flask import Flask, request
 import requests
 # create the Flask app
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/PredictProduct',methods=["Get"])
+@cross_origin()
 def query_example():
     image=request.args.get("image")
     resp=requests.post(f'https://detect.roboflow.com/final-k6z7a/1?api_key=wOakuvyV6yJhqFM3gXDs&image={image}')
@@ -14,4 +17,4 @@ def query_example():
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, port=5000)
